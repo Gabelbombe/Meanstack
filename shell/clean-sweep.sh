ba19d7d79b2ac0c7d90cea4a73ab0fa0fc5784a4
Jd Daniel <dodomeki@gmail.com>
2014-04-07 12:14:49 -0700

    BEN-682: Author backtrack

--- shell/clean-sweep.sh
+++ shell/clean-sweep.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# CPR : Jd Daniel :: http://goo.gl/Yejho4
+# MOD : 2014-04-07 @ 12:09:01
+# VER : 0.2a
 
-cd ~/www/mean/BenApp/
+[ -z "${1}" ] && {
+  cd ~/www/mean/BenApp/
+} || {
+  cd "${1}"
+}
+
 echo -e "\nErasing contents of: $(pwd)"
 read -p "Directory correct [Y/n]: " dir
 
-[ 'y' == "$(echo $dir | awk '{print tolower($0)}')" ] || {
+[ 'y' == "$(echo ${dir} | awk '{print tolower($0)}')" ] || {
   read -p "Enter absolute path: " dir
-  [ -d "$dir" ] && { cd $dir ; }
+  [ -d "$dir" ] && { cd ${dir} ; }
 }
 
 rm -rf *    && git reset --hard HEAD
