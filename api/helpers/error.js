37eef05060d0af9e7fb339c235e8b4e105710e8e
Jd Daniel <dodomeki@gmail.com>
2014-04-10 15:41:42 -0700

    BEN-682: Nuke and pave

--- api/helpers/error.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function(error) {
-  var stack = (error.stack ? error.stack.split("\n") : '');
-  return {
-    name : error.name,
-    message : error.message,
-    stack : stack
-  }
-}
