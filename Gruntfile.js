780340d3e9a388e699a50960bb81857112845184
Jd Daniel <dodomeki@gmail.com>
2014-04-11 11:54:52 -0700

    BEN-682: Task registration note

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,26 @@
 
 module.exports = function(grunt) {
 
-    var path    = require('path'),
-        process = require('process');
+    var process = require('process'),
+        path    = require('path');
 
     require('load-grunt-config')(grunt, {
-        configPath: path.join(process.cwd(), 'grunt'), //path to task.js files, defaults to grunt dir
-        init: true, //auto grunt.initConfig
-        data: { //data passed into config.  Can use with <%= test %>
+        configPath: path.join(process.cwd(), 'grunt'), // path to task.js files, defaults to grunt dir
+        init: true, // auto grunt.initConfig
+        data: {     // data passed into config.  Can use with <%= test %>
             test: false
         },
-        loadGruntTasks: { //can optionally pass options to load-grunt-tasks.  If you set to false, it will disable auto loading tasks.
+        loadGruntTasks: { // can optionally pass options to load-grunt-tasks.  If you set to false, it will disable auto loading tasks.
             pattern: 'grunt-*',
             config:  require('./package.json'),
             scope:   'devDependencies'
         }
     });
+
+    /**
+     * Task registration still outside of autoloader
+     * TODO: [Load Externally defined tasks] (http://gruntjs.com/api/grunt#loading-externally-defined-tasks)
+     */
 
     // making grunt default to force in order not to break the project.
     grunt.option('force', true);
